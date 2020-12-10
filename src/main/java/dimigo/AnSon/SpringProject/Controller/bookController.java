2097dea926de8b37982b2727df928cf401cba89a
YusungAN <52990109+YusungAN@users.noreply.github.com>
2020-12-10 17:32:07 +0900

    add, read 페이지 구현

--- src/main/java/dimigo/AnSon/SpringProject/Controller/bookController.java
+++ src/main/java/dimigo/AnSon/SpringProject/Controller/bookController.java
@@ -3,13 +3,66 @@ package dimigo.AnSon.SpringProject.Controller;
 import dimigo.AnSon.SpringProject.BookForm;
 import dimigo.AnSon.SpringProject.Domain.Book;
 import dimigo.AnSon.SpringProject.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class bookController {
     public int id = 1;
     private BookService bs;
+
+    @Autowired
+    public bookController(BookService bs) {
+        this.bs = bs;
+    }
+
+    @GetMapping("/add")
+    public String AddPage() {
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String PostBook(BookForm bf) {
+        Book b = new Book();
+        System.out.println(bf.getBookName());
+        b.setBookName(bf.getBookName());
+        b.setAuthor(bf.getAuthor());
+        b.setPublicationYear(bf.getPublicationYear());
+        b.setPublisher(bf.getPublisher());
+        int res = bs.join(b);
+        System.out.println(res);
+        if (res != -1) return "redirect:read";
+        else return "error";
+    }
+
+    @GetMapping("/read")
+    public String ReadPage() {
+        return "read";
+    }
+
+    @GetMapping("/search")
+    public String Search(@RequestParam("field") String field, @RequestParam("book_title") String content, Model model) {
+        ArrayList<Book> arr;
+        arr = bs.search(field, content);
+        model.addAttribute("arr", arr);
+        return "read";
+    }
+
+    @GetMapping("/modify")
+    public String ModifyPage() {
+        return "modify";
+    }
+
+    @GetMapping("/delete")
+    public String DeletePage() {
+        return "delete";
+    }
 
     @PostMapping("/books/add")
     public String BookAdd(BookForm bf){
@@ -22,7 +75,7 @@ public class bookController {
 
         bs.join(b);
 
-        return "redirect/";
+        return "redirect:/";
     }
 
 }
--- src/main/java/dimigo/AnSon/SpringProject/Service/BookService.java
+++ src/main/java/dimigo/AnSon/SpringProject/Service/BookService.java
@@ -4,6 +4,8 @@ import dimigo.AnSon.SpringProject.Domain.Book;
 import dimigo.AnSon.SpringProject.Repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+
 public class BookService {
     private BookRepository bookRepository;
 
@@ -13,9 +15,37 @@ public class BookService {
     }
 
     public int join(Book b){
+        try {
+            Book res = bookRepository.save(b);
+            return res.getId();
+        } catch (Exception e) {
+            return -1;
+        }
+    }
 
-        bookRepository.save(b);
-        return b.getId();
+    public ArrayList<Book> search(String field, String content) {
+        switch (field) {
+            case "title":
+                return bookRepository.findByBookName(content);
+            case "author":
+                return bookRepository.findByAuthor(content);
+            case "publisher":
+                return bookRepository.findByPublisher(content);
+            case "publication_year":
+                return bookRepository.findByPubYear(Integer.parseInt(content));
+            default:
+                System.out.println("정보를 조회할 수 없습니다.");
+        }
+        return null;
+    }
+
+    //그냥 일단 만들어둠
+    public void modify(Book book) {
+        bookRepository.changeBookInfo(book);
+    }
+
+    public void delete(Book book) {
+        bookRepository.deleteBook(book);
     }
 
 }
